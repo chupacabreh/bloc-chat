0c32505cf27f0edb1d70484747196e974057a441
Matt Freitas <mattfreitas415@gmail.com>
2017-11-07 20:13:44 -0800

    list message feature added

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,18 +1,26 @@
 (function () {
-    function HomeCtrl(Room, $uibModal) {
+    function HomeCtrl(Room, $uibModal, Message) {
         this.rooms = Room.all;
+        this.currentRoom = null;
+        this.messages = null;
 
         this.addRoom = function (){
-            $uibModal.open({
-                templateUrl: '/templates/modal.html',
-                controller: 'ModalCtrl as modal',
-                size: 'sm'
-            });
-        }
+          $uibModal.open({
+            templateUrl: '/templates/modal.html',
+            controller: 'ModalCtrl as modal',
+            size: 'sm'
+          });
+        };
+
+        this.setCurrentRoom = function(room) {
+          this.currentRoom = room;
+          this.messages = Message.getByRoomId(room.$id);
+        };
+
       }
 
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', '$uibModal', 'Message', HomeCtrl]);
 })();
