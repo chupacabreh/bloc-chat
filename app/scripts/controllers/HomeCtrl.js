f5bb4b095c1f15b8f7b3d3e6ae2ea6d0092fd14e
Matt Freitas <mattfreitas415@gmail.com>
2017-11-17 10:21:57 -0800

    added send message functionalityy

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,17 @@
 (function () {
     function HomeCtrl(Room, $uibModal, Message, $cookies) {
+        this.title = "Messages";
         this.rooms = Room.all;
         this.currentRoom = null;
         this.messages = null;
-        this.username = $cookies.get('blocChatCurrentUser');
+        this.currentUser = $cookies.get('blocChatCurrentUser');
+        this.newMessage = {
+            content: null,
+            roomId: null,
+            sentAt: null,
+            username: null
+        }
+
         this.addRoom = function (){
           $uibModal.open({
             templateUrl: '/templates/modal.html',
@@ -15,6 +23,14 @@
         this.setCurrentRoom = function(room) {
           this.currentRoom = room;
           this.messages = Message.getByRoomId(room.$id);
+        };
+
+        this.sendMessage = function() {
+            this.newMessage.roomId = this.currentRoom.$id;
+            this.newMessage.username = this.currentUser;
+            this.newMessage.sentAt = firebase.database.ServerValue.TIMESTAMP;
+            Message.send(this.newMessage);
+            document.getElementById("textarea").value = "";
         };
 
       }
